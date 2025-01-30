eca160b785c63fa6e57e98653a246de8cedf5bc3
Anastasiia Savinova Occhiminuti <anastesie@hotmail.com>
2025-01-30 16:49:14 +0100

    module 3 lesson 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import React from "react";
+import "modern-normalize";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -20,10 +11,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-
-// import ReactDOM from "react-dom/client";
-// import App from "./components/App/App";
-// import "modern-normalize";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
